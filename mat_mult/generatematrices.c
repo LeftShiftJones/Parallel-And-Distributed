96a06ffba67a3d0034d1655c272bfcf1a4df685d
LeftShiftJones <ryan_jones5@taylor.edu>
2018-10-09 20:22:15 -0400

    Got matrix generator working

--- mat_mult/generatematrices.c
+++ mat_mult/generatematrices.c
@@ -1,18 +1,26 @@
 #include <stdio.h>
 #include <time.h>
+#include <stdlib.h>
 #include "generatematrices.h"
 
-srand(time(0));
+/**
+Matrix generation file,
+"inspired" by Dr. Nurkkala's in-class demonstration
+*/
+
+//srand(time(0));
 
 void generate_matrix(int rows, int cols, char *file_name) {
-    int num_elements = row * cols;
+    srand(time(0));
+    //printf("%d, %d\n", rows, cols);
+    int num_elements = rows*cols;
     int *new_matrix = malloc(num_elements * sizeof(int));
 
     for(int i = 0; i < num_elements; i++) {
-        new_matrix[i] = rand();
+        new_matrix[i] = random() / (RAND_MAX / 100);
     }
 
-    write_matrix(rows, cols, file_name);
+    write_matrix(new_matrix, rows, cols, file_name);
 }
 
 void write_matrix(int *matrix, int rows, int cols, char *file_name) {
@@ -23,13 +31,12 @@ void write_matrix(int *matrix, int rows, int cols, char *file_name) {
         exit(1);
     }
 
-    //print rows + cols to file
     fprintf(fp, "%d %d\n", rows, cols);
     for(int r = 0; r < rows; r++) {
         for(int c = 0; c < cols; c++) {
             fprintf(fp, " %3d", MAT_ELT(matrix, cols, r, c));
         }
-        fprintf("\n"); //newline in file
+        fprintf(fp, "\n");
     }
     fclose(fp);
 }
@@ -37,17 +44,17 @@ void write_matrix(int *matrix, int rows, int cols, char *file_name) {
 int *read_matrix(int *rows, int *cols, char *file_name) {
     FILE *fp;
     if((fp = fopen(file_name, "r")) == NULL) {
-        fprintf(error);
+        fprintf(stderr, "Problem parsing file.\n");
+        exit(1);
     }
 
     fscanf(fp, "%d %d\n", rows, cols); //get rows and columns
-    int num_elements = rows * cols;
+    int num_elements = *rows * *cols;
     int *rtn = malloc(num_elements * sizeof(int));
     for(int i = 0; i < num_elements; i++) {
         fscanf(fp, "%d", rtn+i);
     }
     fclose(fp);
 
-    //give back matrix
     return rtn;
 }
